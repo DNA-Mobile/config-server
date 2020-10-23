cbff579184adb4f65f9d3b434cdff9d16a5e46c2
Danilo Portela <Danilo Portela@host.docker.internal>
2020-10-23 12:18:22 -0300

    disabling registering

--- src/main/java/com/dnamobile/configuration/ConfigServerApplication.java
+++ src/main/java/com/dnamobile/configuration/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.dnamobile.config;
+package com.dnamobile.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
